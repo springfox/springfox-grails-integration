bc755ad56990555285735aa23471d9ff2a579aee
dilipkrish <dilipkrish@users.noreply.github.com>
2017-01-13 17:47:03 -0600

    The action resolver is now greatly simplified
    Now all it needs to do is to delegate to the appropriate factory based on if its a restful controller or not

--- springfox-grails/src/main/java/springfox/documentation/grails/ActionSpecificationResolver.java
+++ springfox-grails/src/main/java/springfox/documentation/grails/ActionSpecificationResolver.java
@@ -1,44 +1,27 @@
 package springfox.documentation.grails;
 
-import com.fasterxml.classmate.TypeResolver;
 import grails.core.GrailsClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
-import springfox.documentation.spring.web.readers.operation.HandlerMethodResolver;
-
-import java.util.HashSet;
-import java.util.Map;
-
-import static springfox.documentation.grails.Actions.*;
 
 @Component
 public class ActionSpecificationResolver {
-  private final GrailsActionAttributes urlProvider;
-  private final HandlerMethodResolver handlerMethodResolver;
-  private final Map<String, ActionSpecificationFactory> restfulActions;
+  private final RestfulActionSpecificationFactory restfulActions;
+  private final MethodBackedActionSpecificationFactory methodBackedActions;
 
   @Autowired
-  public ActionSpecificationResolver(TypeResolver resolver, GrailsActionAttributes urlProvider) {
-    this.urlProvider = urlProvider;
-    this.restfulActions = restfulActions(resolver);
-    this.handlerMethodResolver = new HandlerMethodResolver(resolver);
+  public ActionSpecificationResolver(
+      RestfulActionSpecificationFactory restfulActions,
+      MethodBackedActionSpecificationFactory methodBackedActions) {
+    this.restfulActions = restfulActions;
+    this.methodBackedActions = methodBackedActions;
   }
 
   ActionSpecification resolve(GrailsActionContext context) {
     if (isRestfulController(context.getController())) {
-      return restfulActions.get(context.getAction()).create(context);
+      return restfulActions.create(context);
     }
-    Map<String, HandlerMethod> actions = actionsToHandler(context.getController().getClazz());
-    HandlerMethod handlerMethod = actions.get(context.getAction());
-    return new ActionSpecification(
-        urlProvider.httpMethod(context),
-        new HashSet<>(),
-        new HashSet<>(),
-        handlerMethod,
-        handlerMethodResolver.methodParameters(handlerMethod),
-        handlerMethodResolver.methodReturnType(handlerMethod)
-    );
+    return methodBackedActions.create(context);
   }
 
   @SuppressWarnings({"unchecked", "squid:S1166"})
