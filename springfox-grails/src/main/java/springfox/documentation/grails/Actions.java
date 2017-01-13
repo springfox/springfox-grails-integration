a169c114c8f86ac422f7336b3f61e109241ab46f
dilipkrish <dilipkrish@users.noreply.github.com>
2017-01-13 17:45:14 -0600

    Created factory for restful action specifications
    This encapsulates the conventions of restful actions such as index, create, save, update etc. These are all actions backed by a RestfulController (grails rest plugin)

--- springfox-grails/src/main/java/springfox/documentation/grails/Actions.java
+++ springfox-grails/src/main/java/springfox/documentation/grails/Actions.java
@@ -1,6 +1,5 @@
 package springfox.documentation.grails;
 
-import com.fasterxml.classmate.TypeResolver;
 import grails.web.Action;
 import groovy.lang.GroovyObject;
 import org.springframework.web.method.HandlerMethod;
@@ -8,7 +7,6 @@ import org.springframework.web.method.HandlerMethod;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 class Actions {
@@ -29,18 +27,5 @@ class Actions {
       superClass = superClass.getSuperclass();
     }
     return handlerLookup;
-  }
-
-  static Map<String, ActionSpecificationFactory> restfulActions(TypeResolver resolver) {
-    LinkedHashMap<String, ActionSpecificationFactory> map = new LinkedHashMap<>(8);
-    map.put("index", new IndexActionSpecificationFactory(resolver));
-    map.put("show", new ShowActionSpecificationFactory(resolver));
-    map.put("create", new CreateActionSpecificationFactory(resolver));
-    map.put("edit", new EditActionSpecificationFactory(resolver));
-    map.put("update", new UpdateActionSpecificationFactory(resolver));
-    map.put("save", new SaveActionSpecificationFactory(resolver));
-    map.put("patch", new PatchActionSpecificationFactory(resolver));
-    map.put("delete", new DeleteActionSpecificationFactory(resolver));
-    return map;
   }
 }
---
+++ springfox-grails/src/main/java/springfox/documentation/grails/RestfulActionSpecificationFactory.java
@@ -0,0 +1,36 @@
+package springfox.documentation.grails;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class RestfulActionSpecificationFactory implements ActionSpecificationFactory {
+  private final Map<String, ActionSpecificationFactory> factoryLookup;
+
+  @Autowired
+  public RestfulActionSpecificationFactory(TypeResolver resolver) {
+    this.factoryLookup = restfulActions(resolver);
+  }
+
+  @Override
+  public ActionSpecification create(GrailsActionContext context) {
+    return factoryLookup.get(context.getAction()).create(context);
+  }
+
+  private static Map<String, ActionSpecificationFactory> restfulActions(TypeResolver resolver) {
+    LinkedHashMap<String, ActionSpecificationFactory> map = new LinkedHashMap<>(8);
+    map.put("index", new IndexActionSpecificationFactory(resolver));
+    map.put("show", new ShowActionSpecificationFactory(resolver));
+    map.put("create", new CreateActionSpecificationFactory(resolver));
+    map.put("edit", new EditActionSpecificationFactory(resolver));
+    map.put("update", new UpdateActionSpecificationFactory(resolver));
+    map.put("save", new SaveActionSpecificationFactory(resolver));
+    map.put("patch", new PatchActionSpecificationFactory(resolver));
+    map.put("delete", new DeleteActionSpecificationFactory(resolver));
+    return map;
+  }
+}
