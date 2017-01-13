d7d562de3f2987dd51ba23c5b75f2bc072c0625f
dilipkrish <dilipkrish@users.noreply.github.com>
2017-01-13 17:46:12 -0600

    Created a method backed action specification
    This relies on the method that implements a grails action to infer parameters return types etc.

---
+++ springfox-grails/src/main/java/springfox/documentation/grails/MethodBackedActionSpecificationFactory.java
@@ -0,0 +1,39 @@
+package springfox.documentation.grails;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import springfox.documentation.spring.web.readers.operation.HandlerMethodResolver;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import static springfox.documentation.grails.Actions.*;
+
+@Component
+public class MethodBackedActionSpecificationFactory implements ActionSpecificationFactory {
+
+  private final GrailsActionAttributes urlProvider;
+  private final HandlerMethodResolver handlerMethodResolver;
+
+  @Autowired
+  public MethodBackedActionSpecificationFactory(TypeResolver resolver, GrailsActionAttributes urlProvider) {
+    this.urlProvider = urlProvider;
+    this.handlerMethodResolver = new HandlerMethodResolver(resolver);
+  }
+
+  @Override
+  public ActionSpecification create(GrailsActionContext context) {
+    Map<String, HandlerMethod> actions = actionsToHandler(context.getController().getClazz());
+    HandlerMethod handlerMethod = actions.get(context.getAction());
+    return new ActionSpecification(
+        urlProvider.httpMethod(context),
+        new HashSet<>(),
+        new HashSet<>(),
+        handlerMethod,
+        handlerMethodResolver.methodParameters(handlerMethod),
+        handlerMethodResolver.methodReturnType(handlerMethod)
+    );
+  }
+}
