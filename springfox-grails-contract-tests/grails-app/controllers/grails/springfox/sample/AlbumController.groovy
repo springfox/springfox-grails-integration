b5393930d574f77e305d78dfd556c04b81ad814a
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-10 15:39:21 -0600

    Handled method overrides specified in the controller
    (6)

--- springfox-grails-contract-tests/grails-app/controllers/grails/springfox/sample/AlbumController.groovy
+++ springfox-grails-contract-tests/grails-app/controllers/grails/springfox/sample/AlbumController.groovy
@@ -1,10 +1,9 @@
 package grails.springfox.sample
 
-
-import grails.rest.*
-import grails.converters.*
+import grails.rest.RestfulController
 
 class AlbumController extends RestfulController {
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
     static responseFormats = ['json', 'xml']
     AlbumController() {
         super(Album)
