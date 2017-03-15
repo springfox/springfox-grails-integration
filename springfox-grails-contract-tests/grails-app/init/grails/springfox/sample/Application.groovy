92f6765baebdfc73f3ac1abe7f2fa79b96a1c845
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-14 23:02:00 -0500

    Restored the configuration prior to the breaking build
    Unable to figure out why the configurations are not working. Possibly a grails issue, weird that it works locally

--- springfox-grails-contract-tests/grails-app/init/grails/springfox/sample/Application.groovy
+++ springfox-grails-contract-tests/grails-app/init/grails/springfox/sample/Application.groovy
@@ -1,16 +1,21 @@
 package grails.springfox.sample
 
-import com.fasterxml.classmate.TypeResolver
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import grails.core.GrailsApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import springfox.documentation.grails.DefaultGrailsAlternateTypeRuleConvention
-import springfox.documentation.grails.GrailsSerializationTypeGenerator
+import springfox.documentation.grails.DefaultGeneratedClassNamingStrategy
+import springfox.documentation.grails.DefaultGrailsPropertySelector
+import springfox.documentation.grails.DefaultGrailsPropertyTransformer
+import springfox.documentation.grails.GeneratedClassNamingStrategy
+import springfox.documentation.grails.GrailsPropertySelector
+import springfox.documentation.grails.GrailsPropertyTransformer
 import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
+import springfox.documentation.schema.AlternateTypeRule
+import springfox.documentation.schema.AlternateTypeRuleConvention
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -26,20 +31,34 @@ class Application extends GrailsAutoConfiguration {
   }
 
   @Bean
-  public DefaultGrailsAlternateTypeRuleConvention grailsModelConvention(
-      TypeResolver resolver,
-      GrailsApplication grailsApplication,
-      GrailsSerializationTypeGenerator typeGenerator) {
-    new DefaultGrailsAlternateTypeRuleConvention(resolver, grailsApplication, typeGenerator)
+  @ConditionalOnMissingBean(GrailsPropertySelector)
+  GrailsPropertySelector propertySelector() {
+    new DefaultGrailsPropertySelector()
   }
 
   @Bean
-  public Docket api() {
+  @ConditionalOnMissingBean(GrailsPropertyTransformer)
+  GrailsPropertyTransformer propertyTransformer() {
+    new DefaultGrailsPropertyTransformer()
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(GeneratedClassNamingStrategy)
+  GeneratedClassNamingStrategy namingStrategy() {
+    new DefaultGeneratedClassNamingStrategy()
+  }
+
+  @Bean
+  Docket api(List<AlternateTypeRuleConvention> conventions) {
+    def typeRules = conventions.collectMany {
+      it.rules()
+    }
     new Docket(DocumentationType.SWAGGER_2)
         .ignoredParameterTypes(MetaClass)
         .select()
         .paths(not(ant("/error")))
         .build()
+        .alternateTypeRules(typeRules.toArray(new AlternateTypeRule[typeRules.size()]))
   }
 
   @Bean
--- springfox-grails/src/main/java/springfox/documentation/grails/SpringfoxGrailsIntegrationConfiguration.java
+++ springfox-grails/src/main/java/springfox/documentation/grails/SpringfoxGrailsIntegrationConfiguration.java
@@ -1,29 +1,9 @@
 package springfox.documentation.grails;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "springfox.documentation.grails")
 public class SpringfoxGrailsIntegrationConfiguration {
-
-  @Bean
-  @ConditionalOnMissingBean(GrailsPropertySelector.class)
-  public GrailsPropertySelector propertySelector() {
-    return new DefaultGrailsPropertySelector();
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(GrailsPropertyTransformer.class)
-  public GrailsPropertyTransformer propertyTransformer() {
-    return new DefaultGrailsPropertyTransformer();
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(GeneratedClassNamingStrategy.class)
-  public GeneratedClassNamingStrategy namingStrategy() {
-    return new DefaultGeneratedClassNamingStrategy();
-  }
 }
