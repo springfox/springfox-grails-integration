d521fc0af9f3a5b128831ffe6b624fc539d09cb3
dilipkrish <dilipkrish@users.noreply.github.com>
2017-03-16 19:36:56 -0500

    Restored it back to shared conditional beans
    The reason it was broken is that the changes in springfox were not in bintray/artifactory yet.

--- springfox-grails-contract-tests/grails-app/init/grails/springfox/sample/Application.groovy
+++ springfox-grails-contract-tests/grails-app/init/grails/springfox/sample/Application.groovy
@@ -2,20 +2,11 @@ package grails.springfox.sample
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
-import springfox.documentation.grails.DefaultGeneratedClassNamingStrategy
-import springfox.documentation.grails.DefaultGrailsPropertySelector
-import springfox.documentation.grails.DefaultGrailsPropertyTransformer
-import springfox.documentation.grails.GeneratedClassNamingStrategy
-import springfox.documentation.grails.GrailsPropertySelector
-import springfox.documentation.grails.GrailsPropertyTransformer
 import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration
-import springfox.documentation.schema.AlternateTypeRule
-import springfox.documentation.schema.AlternateTypeRuleConvention
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -31,34 +22,12 @@ class Application extends GrailsAutoConfiguration {
   }
 
   @Bean
-  @ConditionalOnMissingBean(GrailsPropertySelector)
-  GrailsPropertySelector propertySelector() {
-    new DefaultGrailsPropertySelector()
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(GrailsPropertyTransformer)
-  GrailsPropertyTransformer propertyTransformer() {
-    new DefaultGrailsPropertyTransformer()
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(GeneratedClassNamingStrategy)
-  GeneratedClassNamingStrategy namingStrategy() {
-    new DefaultGeneratedClassNamingStrategy()
-  }
-
-  @Bean
-  Docket api(List<AlternateTypeRuleConvention> conventions) {
-    def typeRules = conventions.collectMany {
-      it.rules()
-    }
+  Docket api() {
     new Docket(DocumentationType.SWAGGER_2)
         .ignoredParameterTypes(MetaClass)
         .select()
         .paths(not(ant("/error")))
         .build()
-        .alternateTypeRules(typeRules.toArray(new AlternateTypeRule[typeRules.size()]))
   }
 
   @Bean
